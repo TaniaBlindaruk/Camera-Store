0933735f584828aa674677c3388e9f511d03591e
konstanchuk <vova.konstanchuk@gmail.com>
2014-11-02 00:37:49 +0200

    шапка і меню

--- js/slide_show.js
+++ js/slide_show.js
@@ -4,20 +4,20 @@ var slideImage;
 function slideShow() {
     var docWidth = $(document).width();
     var k = (docWidth-960)/2;
-    slideImageArr  = $('#slider-images > div');
+    slideImageArr  = $($('#slider-images > div')[0]);
     pos = Math.round(slideImageArr.length/2);
     slideImage = $('#slider');
     slideImageArr.css({
-        'left': k + 'px'
+        'margin-left': k + 'px'
     });
     $('#left-arr').click(function() {
         slideImageArr.animate({
-            'left': parseInt(slideImageArr.css('left'))-960 + 'px'
+            'margin-left': parseInt(slideImageArr.css('margin-left'))-960 + 'px'
         });
     });
     $('#right-arr').click(function() {
         slideImageArr.animate({
-            'left': parseInt(slideImageArr.css('left'))+960 + 'px'
+            'margin-left': parseInt(slideImageArr.css('margin-left'))+960 + 'px'
         })
     })
 }
