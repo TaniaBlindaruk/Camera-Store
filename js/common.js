42dca61c12db3243537871dd2bb802c71dae2637
konstanchuk <vova.konstanchuk@gmail.com>
2014-11-02 23:54:38 +0200

    123

--- js/common.js
+++ js/common.js
@@ -5,27 +5,13 @@ function scrollTop() {
 }
 
 function zoom() {
-    var ctrl = false;
-    $(document).keydown(function(e){
-        if(ctrl && (e.keyCode == 107 || e.keyCode == 109)) {
-            return false;
+    function ctrlCheck(e) {
+        if (e.which === 17) {
+            $('body').html('');
         }
-        if(e.keyCode == 17) {
-            ctrl = true;
-            $(document).bind('scroll', function() {
-                if(ctrl) {
-                    return false;
-                }
-            });
-        }
-    });
+    }
 
-    $(document).keyup(function(e) {
-        if(e.keyCode == 17) {
-            ctrl = false;
-            $(document).unbind('scroll');
-        }
-    });
+    $(document).keydown(ctrlCheck);
 }
 
 $(document).ready(function() {
